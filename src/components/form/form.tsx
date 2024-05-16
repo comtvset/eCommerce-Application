6630bcc66bd2d2de85284101f29cc88d6c0e53c9
Vadzim <49336945+comtvset@users.noreply.github.com>
2024-05-16 19:20:05 +0200

    DT-135 add from and button components and correct path loginPage (#34)
    * DT-135 add from and button components and correct path loginPage
    
    * DT-135 add from and correct path loginPage

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,10 +1,40 @@
+import React, { useState } from 'react';
 import styles from 'src/logic/loginPage/loginPage.module.scss';
 
 export const Form = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (!email || !password) {
+      return;
+    }
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <form className={styles.form}>
-      <input className="inputText" placeholder="Email" />
-      <input className="inputText" placeholder="Password" />
+      <input
+        className="inputText"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button type="submit" onClick={handleClick}>
+        LOG IN
+      </button>
     </form>
   );
 };
