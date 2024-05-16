c0abcda4c7fa2e58a20b7722089c1d52d1eb872a
maria.akulova <maria.akulova.dragon@gmail.com>
2024-05-16 17:18:38 +0200

    DT-134 add registration form, button

--- src/components/form/RegistrationForm.tsx
+++ src/components/form/RegistrationForm.tsx
@@ -1,17 +1,142 @@
-import style from 'src/logic/loginPage/loginPage.module.scss';
+import { useState } from 'react';
+import styleLogin from 'src/logic/loginPage/loginPage.module.scss';
+import styleRegistration from 'src/logic/registrationPage/registration.module.scss';
 
 export const RegistrationForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const validateInputs = () => {
+    return false;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateInputs()) {
+      setIsDisabled(false);
+    }
+  };
+
   return (
-    <form className={style.form}>
-      <input className="inputText" placeholder="Email" />
-      <input className="inputText" placeholder="Password" />
-      <input className="inputText" placeholder="First name" />
-      <input className="inputText" placeholder="Last name" />
-      <input className="inputText" placeholder="Date of birth" />
-      <input className="inputText" placeholder="Street" />
-      <input className="inputText" placeholder="City" />
-      <input className="inputText" placeholder="Postal code" />
-      <input className="inputText" placeholder="Country" />
+    <form className={styleLogin.form} onSubmit={handleSubmit} onChange={handleSubmit}>
+      <input
+        className="inputText"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Date of birth"
+        value={dateOfBirth}
+        onChange={(e) => {
+          setDateOfBirth(e.target.value);
+        }}
+      />
+      <h2>Shipping address</h2>
+      <input
+        className="inputText"
+        placeholder="Street"
+        value={street}
+        onChange={(e) => {
+          setStreet(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="City"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Postal code"
+        value={postalCode}
+        onChange={(e) => {
+          setPostalCode(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Country"
+        value={country}
+        onChange={(e) => {
+          setCountry(e.target.value);
+        }}
+      />
+      <h2>Billing address</h2>
+      <input
+        className="inputText"
+        placeholder="Street"
+        value={street}
+        onChange={(e) => {
+          setStreet(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="City"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Postal code"
+        value={postalCode}
+        onChange={(e) => {
+          setPostalCode(e.target.value);
+        }}
+      />
+      <input
+        className="inputText"
+        placeholder="Country"
+        value={country}
+        onChange={(e) => {
+          setCountry(e.target.value);
+        }}
+      />
+
+      <button className={styleRegistration.submitButton} type="submit" disabled={isDisabled}>
+        APPLY
+      </button>
     </form>
   );
 };
--- src/logic/registrationPage/registrationPage.tsx
+++ src/logic/registrationPage/registrationPage.tsx
@@ -1,10 +1,12 @@
 import stylesLogin from 'src/logic/loginPage/loginPage.module.scss';
+import stylesRegistration from 'src/logic/registrationPage/registration.module.scss';
 import { RegistrationForm } from 'src/components/form/RegistrationForm.tsx';
 
 export const RegistrationPage = () => {
   return (
     <div className={stylesLogin.container_login}>
-      <div className={stylesLogin.login}>
+      <div className={stylesRegistration.registration}>
+        <h1>Create Account</h1>
         <RegistrationForm />
       </div>
     </div>
