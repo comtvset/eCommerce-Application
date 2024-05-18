43bbabd8e4f8e59929302ec3adf8feb1363b8228
MARGO06 <118379169+MARGO06@users.noreply.github.com>
2024-05-18 20:35:08 +0300

    add header on login and register pages (#38)

--- src/logic/mainPage/MainPage.tsx
+++ src/logic/mainPage/MainPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Layout } from 'components/layout/Layout.tsx';
-import { Header } from 'src/components/header/Header.tsx';
+import styles from 'src/logic/mainPage/MainPAge.module.scss';
 import style from 'components/layout/Layout.module.scss';
 
 export const Main: React.FC = () => {
   return (
     <Layout className={style.layout}>
-      <Header />
+      <section className={styles.main} />
     </Layout>
   );
 };
