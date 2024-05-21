474f7c9a1696eba86bc18abcba967c8e9aeb9dca
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-05-21 23:47:51 +0200

    DT-121 implement authentication and save token to ls

--- src/services/api/ctpClient.ts
+++ src/services/api/ctpClient.ts
@@ -1,4 +1,4 @@
-import { ctpClient, getCurrentToken, getRefreshToken } from 'src/services/api/BuildClient.ts';
+import { ctpClient } from 'src/services/api/BuildClient.ts';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 
 const PROJECT_KEY: string = import.meta.env.VITE_CTP_PROJECT_KEY as string;
@@ -6,29 +6,12 @@ const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
   projectKey: PROJECT_KEY,
 });
 
-const getProject = () => {
+const getProject = async () => {
   return apiRoot.get().execute();
 };
 
-const getTokens = async () => {
-  try {
-    await getProject();
-    const currentToken = getCurrentToken();
-    const refreshToken = getRefreshToken();
-
-    const tokens = {
-      curToken: currentToken,
-      refToken: refreshToken,
-    };
-
-    localStorage.setItem('userTokens', JSON.stringify(tokens));
-  } catch (error) {
-    // console.error(error); // commit for ESLint
-  }
-};
-
 (async () => {
-  await getTokens();
+  await getProject();
 })();
 
 export const sdkTest = () => {
