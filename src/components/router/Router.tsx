f41475e8ae05b92e93eeab17848cfa00073e710b
MARGO06 <118379169+MARGO06@users.noreply.github.com>
2024-05-25 11:57:40 +0300

    add the profile link (#58)

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -3,6 +3,7 @@ import { Main } from 'src/logic/mainPage/MainPage.tsx';
 import { Login } from 'src/logic/loginPage/LoginPage.tsx';
 import { Error } from 'src/logic/errorPage/ErrorPage.tsx';
 import { Catalog } from 'src/logic/catalogPage/CatalogPage.tsx';
+import { UserProfilePage } from 'src/logic/profilePage/UserProfilePage.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from 'src/components/header/Header.tsx';
 
@@ -16,6 +17,7 @@ export const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/catalog" element={<Catalog />} />
+        <Route path="/profile" element={<UserProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
