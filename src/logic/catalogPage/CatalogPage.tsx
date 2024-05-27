72163fe0eaeccb010bd29ce74485b63d696d30ed
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-05-27 12:13:49 +0200

    DT-151 fetch and display product list using commercetools API

--- src/logic/catalogPage/CatalogPage.tsx
+++ src/logic/catalogPage/CatalogPage.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import style from 'src/logic/catalogPage/CatalogPage.module.scss';
+import { Card } from 'src/components/cards/Cards.tsx';
+import { ProductCatalogData } from '@commercetools/platform-sdk';
+
+export interface IProductData {
+  id: string;
+  masterData: ProductCatalogData;
+}
 
 export const Catalog: React.FC = () => {
-  return <div>Catalog</div>;
+  return (
+    <div className={style.layout}>
+      <div className={style.dots} />
+      <Suspense>
+        <Card />
+      </Suspense>
+    </div>
+  );
 };
