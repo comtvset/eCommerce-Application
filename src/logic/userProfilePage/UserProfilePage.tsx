6ca03476d40066ee0a9bc1c8618c80c2d49d4fa1
maria.akulova <maria.akulova.dragon@gmail.com>
2024-05-27 20:08:15 +0200

    DT-164 fix pathes in imports

--- src/logic/userProfilePage/UserProfilePage.tsx
+++ src/logic/userProfilePage/UserProfilePage.tsx
@@ -1,4 +1,4 @@
-import { UserProfileForm } from 'src/components/form/profile/UserProfileForm';
+import { UserProfileForm } from 'src/components/form/profile/UserProfileForm.tsx';
 
 export const UserProfilePage = () => {
   return <UserProfileForm />;
