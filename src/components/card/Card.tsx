f0e5a597c17aeb0bacb47e8d7f9c19106f0a729f
MARGO06 <118379169+MARGO06@users.noreply.github.com>
2024-05-30 11:08:43 +0300

    DT-160-RSS_10ImplementSlider (#68)
    * create a slider on the product page
    
    * change code for slider, delete the rule in Eslint

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import style from 'src/components/card/Card.module.scss';
+import { IProductData } from 'src/components/cards/Cards.tsx';
 import { Layout } from 'src/components/layout/Layout.tsx';
 import { apiRoot } from 'src/services/api/ctpClient.ts';
 import { Paragraph } from 'src/components/text/Text.tsx';
-import { IProductData } from 'src/components/cards/Cards.tsx';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+interface Image {
+  url: string;
+}
 
 export const CardOne: React.FC = () => {
   const [product, setProduct] = useState<IProductData>();
   const [error, setError] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
   const { id } = useParams<{ id: string }>();
+
+  const handleImageChange = (newImage: Image) => {
+    setSelectedImage(newImage);
+  };
+
+  const isImages = product?.masterData.staged.masterVariant.images ?? [];
+  const isLength = isImages.length > 1;
 
   useEffect(() => {
     if (typeof id !== 'undefined') {
@@ -28,6 +43,21 @@ export const CardOne: React.FC = () => {
     }
   }, [id]);
 
+  useEffect(() => {
+    if (product?.masterData.staged.masterVariant.images?.length) {
+      setSelectedImage(product.masterData.staged.masterVariant.images[0]);
+    }
+  }, [product]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: isImages.length,
+    slidesToScroll: 1,
+  };
+  const { dots, infinite, speed, slidesToShow, slidesToScroll } = settings;
+
   return (
     <Layout className={style.container}>
       {error && <div className={style.error}>{error}</div>}
@@ -35,24 +65,36 @@ export const CardOne: React.FC = () => {
         <div className={style.product}>
           <div className={style.container_images}>
             <div className={style.main_image}>
-              <img
-                className={style.image}
-                src={product.masterData.staged.masterVariant.images?.[0].url}
-                alt={product.masterData.current.name['en-US']}
-              />
+              {selectedImage && (
+                <img
+                  className={style.image}
+                  src={selectedImage.url}
+                  alt={product.masterData.current.name['en-US']}
+                />
+              )}
             </div>
-            <div className={style.images_container}>
-              {product.masterData.staged.masterVariant.images?.map((image) => {
-                return (
+            {isLength && (
+              <Slider
+                dots={dots}
+                infinite={infinite}
+                speed={speed}
+                slidesToShow={slidesToShow}
+                slidesToScroll={slidesToScroll}
+                className={style.images_container}
+                afterChange={(currentSlide: number) => {
+                  handleImageChange(isImages[currentSlide]);
+                }}
+              >
+                {product.masterData.staged.masterVariant.images?.map((image) => (
                   <img
                     key={image.url}
                     className={style.images}
                     src={image.url}
                     alt={product.masterData.current.name['en-US']}
                   />
-                );
-              })}
-            </div>
+                ))}
+              </Slider>
+            )}
           </div>
           <div className={style.info}>
             <Paragraph
