b3c9fafec71281ff36f6bb58530032c87f650fb7
maria.akulova <maria.akulova.dragon@gmail.com>
2024-06-14 12:40:57 +0200

    DT-214 add Shopping Cart page

--- src/logic/basketPage/basketPage.tsx
+++ src/logic/basketPage/basketPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { CartCustomer } from 'src/components/form/cart/CartCustomer.tsx';
 
 const BasketPage: React.FC = () => {
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
-      <div>BasketSSSSSS</div>
+      <CartCustomer />
     </React.Suspense>
   );
 };
