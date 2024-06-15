6e97dc65b71111b92bf5e30680cbd1188de3fe3b
maria.akulova <maria.akulova.dragon@gmail.com>
2024-06-15 20:05:22 +0200

    DT-220 fix display images on Main page

--- src/logic/mainPage/MainPage.tsx
+++ src/logic/mainPage/MainPage.tsx
@@ -1,14 +1,17 @@
 import styles from 'src/logic/mainPage/MainPAge.module.scss';
+import mainImage from 'src/public/main_image2.jpg';
+import promofood from 'src/public/promofood.png';
+import promotoy from 'src/public/toypromo.png';
 
 export const Main: React.FC = () => {
   return (
     <>
       <div className={styles.main_image_wrapper}>
-        <img src="src/public/main_image2.jpg" alt="decor" className={styles.main_image} />
+        <img src={mainImage} alt="decor" className={styles.main_image} />
       </div>
       <div className={styles.promopictures}>
-        <img src="src/public/promofood.png" alt="food promo" className={styles.food_background} />
-        <img src="src/public/toypromo.png" alt="pic promo" className={styles.pic_background} />
+        <img src={promofood} alt="food promo" className={styles.food_background} />
+        <img src={promotoy} alt="pic promo" className={styles.pic_background} />
       </div>
     </>
   );
