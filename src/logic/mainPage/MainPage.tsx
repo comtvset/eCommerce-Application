e1476ddbfa02640fedcb8de643b9ade7920d6643
maria.akulova <maria.akulova.dragon@gmail.com>
2024-06-15 10:22:56 +0200

    DT-220 fix main page layout

--- src/logic/mainPage/MainPage.tsx
+++ src/logic/mainPage/MainPage.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Layout } from 'components/layout/Layout.tsx';
 import styles from 'src/logic/mainPage/MainPAge.module.scss';
-import style from 'components/layout/Layout.module.scss';
 
 export const Main: React.FC = () => {
   return (
     <>
-      <Layout className={style.layout}>
-        <section className={styles.main} />
-      </Layout>
+      <div className={styles.main_image_wrapper}>
+        <img src="src/public/main_image2.jpg" alt="decor" className={styles.main_image} />
+      </div>
       <div className={styles.promopictures}>
         <img src="src/public/promofood.png" alt="food promo" className={styles.food_background} />
         <img src="src/public/promopicture.png" alt="pic promo" className={styles.pic_background} />
