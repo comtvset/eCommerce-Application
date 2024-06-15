b453c262bc98b4e47399d6a7ec773e3095b43cee
maria.akulova <maria.akulova.dragon@gmail.com>
2024-06-15 11:14:07 +0200

    DT-220 refactor cart page layout

--- src/components/form/cart/CartCustomer.tsx
+++ src/components/form/cart/CartCustomer.tsx
@@ -161,15 +161,7 @@ export const CartCustomer: React.FC = () => {
   return (
     <div className={style.cartContainer}>
       <h1>Shopping Cart</h1>
-      <button
-        type="button"
-        onClick={() => {
-          setShowModal(true);
-        }}
-        className={style.clearCartButton}
-      >
-        Clear Shopping Cart
-      </button>
+
       <Modal
         show={showModal}
         onClose={() => {
@@ -183,80 +175,85 @@ export const CartCustomer: React.FC = () => {
         title="Clear Shopping Cart"
         message="Are you sure you want to clear the shopping cart?"
       />
-
-      <table className={style.cartTable}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <tr key={item.id}>
-              <td>{getLocalizedName(item.name, 'en-US')}</td>
-              <td>
-                {item.variant.images && item.variant.images.length > 0 ? (
-                  <img
-                    src={item.variant.images[0].url}
-                    alt={getLocalizedName(item.name, 'en-US')}
-                    className={style.productImage}
-                  />
-                ) : (
-                  <span>No image available</span>
-                )}
-              </td>
-              <td>
-                {item.price.discounted
-                  ? item.price.discounted.value.centAmount / 100
-                  : item.price.value.centAmount / 100}
-                {` ${item.price.value.currencyCode}`}
-              </td>
-              <td>
-                <label htmlFor="countItem">
-                  {' '}
-                  <input
-                    id="countItem"
-                    type="number"
-                    min="1"
-                    value={item.quantity}
-                    onChange={(e) => {
-                      handleQuantityChange(item.id, parseInt(e.target.value, 10)).catch(
-                        (error: unknown) => {
-                          proceedExceptions(error, 'Modify item quantity failed');
-                        },
-                      );
-                    }}
-                    className={style.quantityInput}
-                  />
-                </label>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={() => {
-                    handleDelete(item.id).catch((error: unknown) => {
-                      proceedExceptions(error, 'Edit address failed');
-                    });
-                  }}
-                  className={style.deleteButton}
-                >
-                  Delete
-                </button>
-              </td>
+      <div className={style.wrapper}>
+        <table className={style.cartTable}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className={style.totalPrice}>
-        <h3>
-          Total:
-          {totalPrice && totalPrice.centAmount / 100}
-          {` ${totalPrice?.currencyCode ?? ''}`}
-        </h3>
+          </thead>
+          <tbody>
+            {cartItems.map((item) => (
+              <tr key={item.id}>
+                <td>{getLocalizedName(item.name, 'en-US')}</td>
+                <td>
+                  {item.variant.images && item.variant.images.length > 0 ? (
+                    <img
+                      src={item.variant.images[0].url}
+                      alt={getLocalizedName(item.name, 'en-US')}
+                      className={style.productImage}
+                    />
+                  ) : (
+                    <span>No image available</span>
+                  )}
+                </td>
+                <td>
+                  {item.price.discounted
+                    ? item.price.discounted.value.centAmount / 100
+                    : item.price.value.centAmount / 100}
+                  {` ${item.price.value.currencyCode}`}
+                </td>
+                <td>
+                  <label htmlFor="countItem">
+                    {' '}
+                    <input
+                      id="countItem"
+                      type="number"
+                      min="1"
+                      value={item.quantity}
+                      onChange={(e) => {
+                        handleQuantityChange(item.id, parseInt(e.target.value, 10)).catch(
+                          (error: unknown) => {
+                            proceedExceptions(error, 'Modify item quantity failed');
+                          },
+                        );
+                      }}
+                      className={style.quantityInput}
+                    />
+                  </label>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      handleDelete(item.id).catch((error: unknown) => {
+                        proceedExceptions(error, 'Edit address failed');
+                      });
+                    }}
+                    className={style.deleteButton}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className={style.right_column}>
+          <button
+            type="button"
+            onClick={() => {
+              setShowModal(true);
+            }}
+            className={style.clearCartButton}
+          >
+            Clear Shopping Cart
+          </button>
+        </div>
       </div>
     </div>
   );
