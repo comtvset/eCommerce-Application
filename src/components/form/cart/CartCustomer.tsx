1b2a97ea63f908e2f99e296e09182740fc1b2559
Margarita Masko <margaritam996@gmail.com>
2024-06-15 14:28:59 +0300

    add the catalog link to the cart page

--- src/components/form/cart/CartCustomer.tsx
+++ src/components/form/cart/CartCustomer.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import style from 'src/components/form/cart/CartCustomer.module.scss';
 import { ServerError } from 'src/utils/error/RequestErrors.ts';
 import { createApiRoot, createLoginApiRoot } from 'src/services/api/BuildClient.ts';
-import { Paragraph } from 'src/components/text/Text.tsx';
 import {
   Cart,
   CentPrecisionMoney,
@@ -11,6 +10,7 @@ import {
   LocalizedString,
 } from '@commercetools/platform-sdk';
 import useModalEffect from 'src/components/form/profile/UseModalEffect.ts';
+import { Message } from 'src/components/cartMessage/CartMessage.tsx';
 
 export const CartCustomer: React.FC = () => {
   const [cartItems, setCartItems] = useState<LineItem[]>([]);
@@ -118,25 +118,13 @@ export const CartCustomer: React.FC = () => {
   };
 
   if (!localStorage.getItem('cartId')) {
-    return (
-      <Paragraph
-        tag="p"
-        className={style.cart__empty}
-        title="Your cart is currently empty, but that's an easy fix! Head over to our store and fill it up with the items you'd like. We're waiting for you!"
-      />
-    );
+    return <Message />;
   }
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (cartItems.length === 0) {
-    return (
-      <Paragraph
-        tag="p"
-        className={style.cart__empty}
-        title="Your cart is currently empty, but that's an easy fix! Head over to our store and fill it up with the items you'd like. We're waiting for you!"
-      />
-    );
+    return <Message />;
   }
 
   return (
--- src/services/api/ApiBasket.ts
+++ src/services/api/ApiBasket.ts
@@ -4,14 +4,14 @@ import { CartUpdate } from '@commercetools/platform-sdk';
 const apiRoot = createApiRoot();
 
 export const createAnonymousBasket = async () => {
-  let idCard = localStorage.getItem('cartId');
-  if (!idCard) {
+  let idCart = localStorage.getItem('cartId');
+  if (!idCart) {
     const body = { country: 'US', currency: 'EUR' };
     const result = await apiRoot.carts().post({ body }).execute();
-    idCard = result.body.id;
+    idCart = result.body.id;
     localStorage.setItem('cartId', result.body.id);
   }
-  return idCard;
+  return idCart;
 };
 
 export const getProducts = async (idCart: string, id: string) => {
