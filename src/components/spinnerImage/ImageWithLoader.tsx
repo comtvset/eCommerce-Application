91804563f1ad229213ac3f5f0b5318d13c2c322a
Maria <99416741+maria-akulova@users.noreply.github.com>
2024-06-19 17:12:14 +0200

    DT-253_ImproveLazyLoadingCatalogPage (#125)
    * DT-253 add observer for lazy loading of visible products on the page
    
    * DT-253 fix linter error
    
    * DT-253 improve Cards layout
    
    * DT-253 improve filters layout

--- src/components/spinnerImage/ImageWithLoader.tsx
+++ src/components/spinnerImage/ImageWithLoader.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Spinner from 'src/components/spinnerImage/SpinnerImage.tsx';
 import style from 'src/components/spinnerImage/ImageWithLoader.module.scss';
 
@@ -10,13 +10,42 @@ interface ImageWithLoaderProps {
 
 const ImageWithLoader: React.FC<ImageWithLoaderProps> = ({ src, alt, className }) => {
   const [loading, setLoading] = useState(true);
+  const [loadedSrc, setLoadedSrc] = useState('');
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setLoadedSrc(src);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { rootMargin: '100px' },
+    );
+
+    const currentImgRef = imgRef.current;
+
+    if (currentImgRef) {
+      observer.observe(currentImgRef);
+    }
+
+    return () => {
+      if (currentImgRef) {
+        observer.unobserve(currentImgRef);
+      }
+    };
+  }, [src]);
 
   return (
     <div className={style.imageContainer}>
       {loading && <Spinner />}
       <img
+        ref={imgRef}
         className={className}
-        src={src}
+        src={loadedSrc}
         alt={alt}
         onLoad={() => {
           setLoading(false);
